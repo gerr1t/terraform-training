9ff5e507e0c01bb54cea2d1a16c0f5c0e376e394
Gerrit Tamboer <gerrit.tamboer@tamboer.it>
2021-06-03 13:54:30 +0200

    Updated to support Linux

--- exercise-6/ec2.tf
+++ exercise-6/ec2.tf
@@ -1,45 +1,45 @@
 # This data source searches for the AMI in AWS, and returns the AMI ID
-data "aws_ami" "windows" {
+data "aws_ami" "linux" {
   most_recent = true
 
   filter {
     name   = "name"
-    values = ["*WindowsTrainingAMI*"]
+    values = ["*terraform-training*"]
   }
 }
 
-resource "aws_instance" "windows" {
-  ami           = "${data.aws_ami.windows.id}"
+resource "aws_instance" "linux" {
+  ami           = data.aws_ami.linux.id
   instance_type = "t2.medium"
 
   # Associating the security group and making sure the instance is started in the correct subnet.
-  vpc_security_group_ids = ["${aws_security_group.windows.id}"]
+  vpc_security_group_ids = [aws_security_group.linux.id]
   subnet_id              = "subnet-01ef24cb191338867"
 
   tags {
     Name = "Gerrit Tamboer"
   }
 
-  # This provisioner copies the test file to the instance 
+  # This provisioner copies the test file to the instance
   provisioner "file" {
     source      = "testfile"
-    destination = "C:/Users/training/Documents/testfile"
+    destination = "/home/fullstaq/testfile"
 
     connection {
-      type     = "winrm"
-      user     = "training"
-      password = "Welkom123!"
+      type     = "ssh"
+      user     = "fullstaq"
+      password = "training"
 
-      # set to 15m to avoid winrm timeout
+      # set to 15m to avoid timeout
       timeout = "15m"
     }
   }
 }
 
-output "windows_public_ip" {
-  value = "${aws_instance.windows.public_ip}"
+output "linux_public_ip" {
+  value = aws_instance.linux.public_ip
 }
 
-output "windows_instance_id" {
-  value = "${aws_instance.windows.id}"
+output "linux_instance_id" {
+  value = aws_instance.linux.id
 }
