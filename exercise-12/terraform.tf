c68317b19a15bbe3cace2263d7ffb28c45c0660c
Gerrit Tamboer <gerrit.tamboer@tamboer.it>
2021-06-11 09:25:32 +0200

    Fixed a bunch of stuff

--- exercise-12/terraform.tf
+++ exercise-12/terraform.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "= 0.11.2"
 
   backend "s3" {
-    bucket     = "terraform-training-state"
+    bucket     = "terraform-training-statefiles"
     key        = "gerrit.tfstate"
     region     = "eu-west-1"
   }
